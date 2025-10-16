d826260173063925a1faf51774aa8d4caefa403d
zurira <20243ds036@utez.edu.mx>
2025-10-15 21:37:33 -0600

    Se creo la funcion agregarTarea, la cual crea etiquetas li, input de checkbox con su respectivo label

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,29 @@
 const btnAgregar = document.querySelector(".btnAgregar");
 const nuevaTarea = document.querySelector(".nuevaTarea");
-const listaTareas = document.querySelector(".tareas");
+const tareas = document.querySelector(".tareas");
 
+
+
+btnAgregar.addEventListener("click", () => {
+  const texto = nuevaTarea.value.trim();
+  if (texto !== "") {
+  
+  }
+});
+
+//cada que se agregue una tarea se creara un elemento li, un input de checkbox con su respectivo label
+function agregarTarea(texto, id) {
+  const li = document.createElement("li");
+
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+  checkbox.id = id;
+
+  const label = document.createElement("label");
+  label.htmlFor = id;
+  label.textContent = texto;
+
+  li.appendChild(checkbox);
+  li.appendChild(label);
+  tareas.appendChild(li);
+}
